179f2eb5f85b07cae0e3cdec6caa8b6e89fbaa17
James D <jtd.programming@gmail.com>
2022-01-25 21:04:17 -0500

    file tree updated

---
+++ index.js
@@ -0,0 +1,11 @@
+//const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./dist/index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
